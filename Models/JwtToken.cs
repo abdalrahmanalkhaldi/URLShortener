3ab442652e68d20eb523b413ffa004b74acee520
abdalrahmanalkhaldi <abdalrahmanalkhaldi2004@gmail.com>
2025-07-16 04:29:25 +0300

    adding the new jwt token model

---
+++ Models/JwtToken.cs
@@ -0,0 +1,12 @@
+﻿namespace URLShortenerApiApplication.Models
+{
+    public class JwtToken
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+
+        public int Liftime { get; set; }
+        public string SigningKey { get; set; }
+
+    }
+}
