f0cb10606506c5e0480fa76ec8a95206d7988d96
abdalrahmanalkhaldi <abdalrahmanalkhaldi2004@gmail.com>
2025-07-19 07:03:54 +0300

    adding these too

---
+++ Services/UrlBackgroundJob/UrlCleanupService.cs
@@ -0,0 +1,39 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Http.HttpResults;
+using URLShortenerApiApplication.Data;
+
+namespace URLShortenerApiApplication.Services.UrlBackgroundJob
+{
+    public class UrlCleanupService : BackgroundService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public UrlCleanupService(IServiceScopeFactory serviceScope)
+        {
+            _scopeFactory = serviceScope;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+
+                using (var scope = _scopeFactory.CreateScope())
+                {
+                    var _context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                    var tt = _context.Urls.Where(u => u.ExpireAt <= DateTime.UtcNow).ToList();
+                    if (tt.Any())
+                    {
+                        _context.Urls.RemoveRange(tt);
+                        await _context.SaveChangesAsync();
+                    }
+                }
+
+                await Task.Delay(5000);
+
+            }
+        }
+    }
+
+}
